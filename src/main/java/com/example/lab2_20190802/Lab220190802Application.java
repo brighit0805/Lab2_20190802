b2c13e7dc87ff8283050c53497d1c2a06ae4ff21
brighit0805 <a20190802@pucp.edu.pe>
2024-08-29 19:21:17 -0500

    hola

---
+++ src/main/java/com/example/lab2_20190802/Lab220190802Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20190802;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220190802Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220190802Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab2_20190802/Lab220190802ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20190802;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220190802ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
